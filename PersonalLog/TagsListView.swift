4d0a0bfebc53e773876b360b868a7ad3a9e32273
Joseph Wardell <jaywardell@mac.com>
2022-12-04 11:26:17 -0500

    TagsListView can allow creation of new tags via an alert with a text field (not ideal, but functional)

--- PersonalLog/TagsListView.swift
+++ PersonalLog/TagsListView.swift
@@ -10,23 +10,50 @@ import SwiftUI
 struct TagsListView: View {
     
     let prompt: String
-    let tags: [String]
+    @State private var tags: [String]
     
     let showCancelButton: Bool
 
     let tagsWereChanged: ([String])->()
-    
+
+    @State private var presentNewTagPrompt = false
+    @State private var newTag = ""
     @Environment(\.dismiss) var dismiss
 
+    init(prompt: String, tags: [String], showCancelButton: Bool,  tagsWereChanged: @escaping ([String])->()) {
+        self.prompt = prompt
+        self.showCancelButton = showCancelButton
+        self.tagsWereChanged = tagsWereChanged
+
+        _tags = .init(initialValue: tags)
+    }
+    
     var body: some View {
         NavigationStack {
-            List(tags, id: \.self) { tag in
-                Text(tag)
-                    .font(.body)
+            List {
+                ForEach(tags, id: \.self) { tag in
+                    Text(tag)
+                        .font(.body)
+                }
+                
+                Button(action: userTappedAddButton) {
+                    Image(systemName: "plus")
+                        .font(.body)
+                        .bold()
+                }
             }
             .listStyle(.plain)
             .navigationTitle(prompt)
             .navigationBarItems(leading: cancelButton, trailing: doneButton)
+            .alert("New Tag", isPresented: $presentNewTagPrompt, actions: {
+                // Any view other than Button would be ignored
+                TextField("new tag", text: $newTag)
+            }, message: {
+                // Any view other than Text would be ignored
+                Text("enter the name of the new tag")
+            })
+            .onChange(of: presentNewTagPrompt, perform: newTagPromptWasPresented)
+
         }
     }
     
@@ -50,6 +77,21 @@ struct TagsListView: View {
         .buttonStyle(.borderedProminent)
     }
 
+    private func userTappedAddButton() {
+        presentNewTagPrompt = true
+    }
+    
+    private func newTagPromptWasPresented(_ presented: Bool) {
+        guard !presented else { return }
+        guard !newTag.isEmpty else { return }
+        
+        withAnimation {
+            tags.append(newTag)
+        }
+        
+        
+        newTag = ""
+    }
 }
 
 struct TagsListView_Previews: PreviewProvider {
