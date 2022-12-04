d030021f1b32269679b80bf1ef921699359ec6b8
Joseph Wardell <jaywardell@mac.com>
2022-12-04 06:02:17 -0500

    created a TopViewController which will act as the top view controller for the app. set the tab view controller in the Storyboard to TopViewController

---
+++ PersonalLog/TopViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TopViewController.swift
+//  PersonalLog
+//
+//  Created by Joseph Wardell on 12/4/22.
+//
+
+import UIKit
+
+class TopViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.isHidden = true
+    }
+}
