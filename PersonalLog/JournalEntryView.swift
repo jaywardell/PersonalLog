5fa2bcb7001c99adec330f2b01077a543b55fa66
Joseph Wardell <jaywardell@mac.com>
2022-12-04 06:22:16 -0500

    added a JournalEntryView for editing journal entries. TopViewController uses a JournalEntryView as its first view controller

---
+++ PersonalLog/JournalEntryView.swift
@@ -0,0 +1,21 @@
+//
+//  JournalEntryView.swift
+//  PersonalLog
+//
+//  Created by Joseph Wardell on 12/4/22.
+//
+
+import SwiftUI
+
+struct JournalEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JournalEntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        JournalEntryView()
+            .previewLayout(.sizeThatFits)
+    }
+}
--- PersonalLog/TopViewController.swift
+++ PersonalLog/TopViewController.swift
@@ -6,17 +6,19 @@
 //
 
 import UIKit
+import SwiftUI
 
 class TopViewController: UITabBarController {
 
     private let journalVC = JournalViewController(style: .grouped)
     private lazy var historyVC = UINavigationController(rootViewController: journalVC)
+    private lazy var journalEntryVC = UIHostingController(rootView: JournalEntryView())
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tabBar.isHidden = true
         
-        self.viewControllers = [historyVC]
+        self.viewControllers = [journalEntryVC, historyVC]
     }
 }
