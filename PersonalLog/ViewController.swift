50c0f2abd5e73e5d5fd8833b11063ca01c1b1ded
Joseph Wardell <jaywardell@mac.com>
2022-12-04 05:50:57 -0500

    Initial Commit

---
+++ PersonalLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalLog
+//
+//  Created by Joseph Wardell on 12/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
