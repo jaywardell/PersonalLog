4557a4b69d95c38485cc29ff4c4ed8bf0d733d06
Joseph Wardell <jaywardell@mac.com>
2022-12-04 15:18:52 -0500

    cleanup

--- PersonalLog/JournalViewController.swift
+++ PersonalLog/JournalViewController.swift
@@ -27,9 +27,6 @@ class JournalViewController: UITableViewController {
 
         self.title = "Personal Log"
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
