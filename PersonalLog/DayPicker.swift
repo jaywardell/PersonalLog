46e8bc5db3622ca2db39d2dbbc36a3ab1ca0788b
Joseph Wardell <jaywardell@mac.com>
2022-12-06 12:34:42 -0500

    tapping the calendar button in the toolbar brings up a SwiftUI calendar view from which the user can choose a date

---
+++ PersonalLog/DayPicker.swift
@@ -0,0 +1,34 @@
+//
+//  DayPicker.swift
+//  PersonalLog
+//
+//  Created by Joseph Wardell on 12/6/22.
+//
+
+import SwiftUI
+
+struct DayPicker: View {
+    
+    @State var selectedDates: Set<DateComponents> = []
+
+    let dayWasChosen: (Date)->()
+    
+    var body: some View {
+        MultiDatePicker("Pick a Day", selection: $selectedDates)
+            .padding(.horizontal)
+            .background(Color(uiColor: .secondarySystemBackground))
+            .onChange(of: selectedDates) { newValue in
+                if let firstComponents = selectedDates.first,
+                let day = Calendar.current.date(from: firstComponents) {
+                    dayWasChosen(day)
+                }
+                selectedDates = []
+            }
+    }
+}
+
+struct DayPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        DayPicker() { _ in }
+    }
+}
--- PersonalLog/TopViewController.swift
+++ PersonalLog/TopViewController.swift
@@ -14,12 +14,13 @@ class TopViewController: UIViewController {
     private lazy var journalVC: JournalViewController = { JournalViewController(data: logic, routes: logic) }()
     private lazy var historyVC = UINavigationController(rootViewController: journalVC)
     
+    private var dayPickerHidden: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let topbar = Toolbar(searchButtonTapped: {}, calendarButtonTapped: {}, tagsButtonTapped: {}, addButtonTapped: journalVC.createNewEntry)
+        let topbar = Toolbar(searchButtonTapped: {}, calendarButtonTapped: showDayPicker, tagsButtonTapped: {}, addButtonTapped: journalVC.createNewEntry)
         let topBarVC = UIHostingController(rootView: topbar)
-        
         let toolbar = topBarVC.view!
         
         // if the contents of the toolbar grow outside its frame
@@ -31,8 +32,19 @@ class TopViewController: UIViewController {
         toolbar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         toolbar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         toolbar.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-
         
+        let dayPicker = DayPicker(dayWasChosen: dayWasChosen)
+        let dayPickerVC = UIHostingController(rootView: dayPicker)
+        let dayPickerView = dayPickerVC.view!
+        dayPickerVC.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(dayPickerView)
+        
+        dayPickerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        dayPickerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        dayPickerView.bottomAnchor.constraint(equalTo: toolbar.topAnchor).isActive = true
+        dayPickerHidden = dayPickerView.heightAnchor.constraint(equalToConstant: 0)
+        dayPickerHidden.isActive = true
+
         historyVC.willMove(toParent: self)
         self.addChild(historyVC)
         historyVC.view.translatesAutoresizingMaskIntoConstraints = false
@@ -41,8 +53,23 @@ class TopViewController: UIViewController {
         historyVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         historyVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         historyVC.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        historyVC.view.bottomAnchor.constraint(equalTo: toolbar.topAnchor).isActive = true
+        historyVC.view.bottomAnchor.constraint(equalTo: dayPickerView.topAnchor).isActive = true
 
         historyVC.didMove(toParent: self)
+    }
+    
+    private func showDayPicker() {
+        dayPickerHidden.isActive.toggle()
+        
+        view.setNeedsUpdateConstraints()
+
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    private func dayWasChosen(_ day: Date) {
+        showDayPicker()
+        
     }
 }
