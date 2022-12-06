6d72640b0f17b5ec1caaf3533d2dd1f2aa053e79
Joseph Wardell <jaywardell@mac.com>
2022-12-06 10:51:47 -0500

    TopViewController is now a container view that holds just the JournalViewController and a custom toolbar

--- PersonalLog/TopViewController.swift
+++ PersonalLog/TopViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SwiftUI
 
-class TopViewController: UITabBarController {
+class TopViewController: UIViewController {
 
     private let logic = Journal()
     private lazy var journalVC: JournalViewController = { JournalViewController(data: logic, routes: logic) }()
@@ -17,8 +17,27 @@ class TopViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tabBar.isHidden = true
+        let toolbar = UIView()
+        toolbar.backgroundColor = .purple
+        toolbar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(toolbar)
         
-        self.viewControllers = [historyVC]
+        toolbar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        toolbar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        toolbar.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        toolbar.heightAnchor.constraint(equalToConstant: 100).isActive = true
+
+        
+        historyVC.willMove(toParent: self)
+        self.addChild(historyVC)
+        historyVC.view.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(historyVC.view)
+        
+        historyVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        historyVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        historyVC.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        historyVC.view.bottomAnchor.constraint(equalTo: toolbar.topAnchor).isActive = true
+
+        historyVC.didMove(toParent: self)
     }
 }
