c9179966ab60bf8cac110cb168e5e84abc49f24f
Joseph Wardell <jaywardell@mac.com>
2022-12-07 13:35:12 -0500

    lots of cleanup/refactoring/rearranging/organizing

--- PersonalLog/TopViewController.swift
+++ PersonalLog/TopViewController.swift
@@ -16,18 +16,24 @@ class TopViewController: UIViewController {
     private lazy var journalVC: JournalViewController = { JournalViewController(data: journal, routes: journal, userTappedContent: toggleToolbar) }()
     private lazy var historyVC = UINavigationController(rootViewController: journalVC)
     private lazy var toolbarVC: UIViewController = {
-        let toolbar = Toolbar(calendarButtonTapped: toggleDayPicker, addButtonTapped: addButtonTapped)
-        let out = UIHostingController(rootView: toolbar)
-        return out
+        let view = Toolbar(calendarButtonTapped: toggleDayPicker, addButtonTapped: addButtonTapped)
+        return UIHostingController(rootView: view)
     }()
+    private lazy var dayPickerVC: UIViewController = {
+        let view = DayPicker(dayWasChosen: dayWasChosen)
+        return UIHostingController(rootView: view)
+    }()
+
+    private var toolbar: UIView { toolbarVC.view! }
+    private var dayPickerView: UIView { dayPickerVC.view! }
+    private var historyView: UIView { historyVC.view! }
 
     private var dayPickerHidden: NSLayoutConstraint!
     private var toolbarHidden: NSLayoutConstraint!
 
     private var subscriptions = Set<AnyCancellable>()
-    
-//    private var toolbar: UIView!
-    private var dayPickerView: UIView!
+        
+    // MARK: - 
     
     init(journal: Journal) {
         self.journal = journal
@@ -40,30 +46,36 @@ class TopViewController: UIViewController {
     }
     
     
-    private var toolbar: UIView { toolbarVC.view! }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         
+        [toolbarVC, dayPickerVC, historyVC].forEach { add(viewController: $0) }
+        
+        layoutChildren()
+
+        listenForKeyboardEvents()
+    }
+        
+    // MARK: - Layout
+    
+    private func layoutChildren() {
+        
+        [toolbar, dayPickerView, historyView]
+            .forEach {
+                $0.translatesAutoresizingMaskIntoConstraints = false
+            }
+
         // if the contents of the toolbar grow outside its frame
         // they should overlay other content (e.g. the table view)
         toolbar.layer.zPosition = 1000
-        toolbar.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(toolbar)
         
         toolbar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         toolbar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         toolbar.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         toolbarHidden = toolbar.heightAnchor.constraint(equalToConstant: toolbar.intrinsicContentSize.height)
         toolbarHidden.isActive = false
-
-        let dayPicker = DayPicker(dayWasChosen: dayWasChosen)
-        let dayPickerVC = UIHostingController(rootView: dayPicker)
-        self.dayPickerView = dayPickerVC.view!
-        dayPickerVC.view.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(dayPickerView)
         
         dayPickerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         dayPickerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
@@ -71,26 +83,14 @@ class TopViewController: UIViewController {
         dayPickerHidden = dayPickerView.heightAnchor.constraint(equalToConstant: 0)
         dayPickerHidden.isActive = true
         dayPickerView.layer.opacity = 0
-
-        historyVC.willMove(toParent: self)
-        self.addChild(historyVC)
-        historyVC.view.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(historyVC.view)
         
         historyVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         historyVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         historyVC.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         historyVC.view.bottomAnchor.constraint(equalTo: dayPickerView.topAnchor).isActive = true
-
-        historyVC.didMove(toParent: self)
-        
-        NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification).sink(receiveValue: keyboardWillAppear)
-        .store(in: &subscriptions)
-
-        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification).sink(receiveValue: keyboardWillDisappear)
-        .store(in: &subscriptions)
     }
     
+    // MARK: - Day Picker
     private var dayPickerVisible: Bool { !dayPickerHidden.isActive }
     private func setDayPickerVisible(_ visible: Bool) {
         dayPickerHidden.isActive = !visible
@@ -119,16 +119,7 @@ class TopViewController: UIViewController {
         journalVC.scrollToEntriesforDay(of: day, animated: true)
     }
     
-    private func keyboardWillAppear(_ unused: Any) {
-        toolbarHidden.isActive = true
-        toolbar.layer.opacity = 0
-    }
-    
-    private func keyboardWillDisappear(_ unused: Any) {
-        toolbarHidden.isActive = false
-        toolbar.layer.opacity = 1
-    }
-    
+    // MARK: - Toolbar
     private func toggleToolbar() {
         guard !dayPickerVisible else {
             toggleDayPicker()
@@ -145,6 +136,8 @@ class TopViewController: UIViewController {
         }
     }
 
+    // MARK: - Responding to user input
+    
     private func addButtonTapped() {
         setDayPickerVisible(false)
         journalVC.showNewEntryChrome()
@@ -153,5 +146,38 @@ class TopViewController: UIViewController {
     private func searchButtonTapped() {
         setDayPickerVisible(false)
         journalVC.showSearchChrome()
+    }
+
+
+    // MARK: - TopViewController: Listening for Keyboard
+
+    private func listenForKeyboardEvents() {
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification).sink(receiveValue: keyboardWillAppear)
+        .store(in: &subscriptions)
+
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification).sink(receiveValue: keyboardWillDisappear)
+        .store(in: &subscriptions)
+    }
+
+    private func keyboardWillAppear(_ unused: Any) {
+        toolbarHidden.isActive = true
+        toolbar.layer.opacity = 0
+    }
+    
+    private func keyboardWillDisappear(_ unused: Any) {
+        toolbarHidden.isActive = false
+        toolbar.layer.opacity = 1
+    }
+}
+
+// MARK: - UIViewController: Child View Controllers
+
+extension UIViewController {
+    func add(viewController: UIViewController) {
+        viewController.willMove(toParent: self)
+        self.addChild(viewController)
+        self.view.addSubview(viewController.view)
+        
+        viewController.didMove(toParent: self)
     }
 }
